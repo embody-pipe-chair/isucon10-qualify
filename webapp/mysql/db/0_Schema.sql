550cf9a9adb56adec80c5f621d8c519d96b6540e
Kwappa Penguin <kp@example.com>
2020-09-12 09:19:41 +0000

    add features_bit

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -16,7 +16,8 @@ CREATE TABLE isuumo.estate
     door_height INTEGER             NOT NULL,
     door_width  INTEGER             NOT NULL,
     features    VARCHAR(64)         NOT NULL,
-    popularity  INTEGER             NOT NULL
+    popularity  INTEGER             NOT NULL,
+    features_bit BIGINT(64) unsigned not null default '0'
 );
 
 CREATE TABLE isuumo.chair
